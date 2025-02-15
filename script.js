1f5a15a936dc280a644906a0af567c61efeae955
SamGomes1984 <127795174+SamGomes1984@users.noreply.github.com>
2025-02-15 17:30:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Initialize Supabase
-const SUPABASE_URL = 'https://your-supabase-url.supabase.co';
-const SUPABASE_API_KEY = 'your-supabase-api-key';
+const SUPABASE_URL = 'https://sgcxoxqgkpkwdxhvwocp.supabase.co';
+const SUPABASE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNnY3hveHFna3Brd2R4aHZ3b2NwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2MjA4MDcsImV4cCI6MjA1NTE5NjgwN30.q7P8HJY-erOW7AbFmDEqx9xMOdjEs6DMThO1UVZRc5g';
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_API_KEY);
 
 // Registration
